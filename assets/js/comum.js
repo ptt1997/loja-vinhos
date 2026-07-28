a877eef2ade3146317c4d9f50678e4fbbffad00f
marcelocc5 <51063057+marcelocc5@users.noreply.github.com>
2026-07-28 19:43:27 -0300

    link
    link novo

--- assets/js/comum.js
+++ assets/js/comum.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiWZu-jpb9Cu1v0fimD4TAzS-LPdNqGYTaY7Q5gQ95Gk_UVRU0s3fCb5BjseDNusVj0nGZoUTy7ejp-RvTB8prPRLIO-hD71oUE2kiDeSeOoUHCJmKWI9flKqtQvpCzHEcQWnSm--FexvVtielgt5Sxop-M7LUY1-ko-Kl1rfzpFMC-S9C2WBQXDJ8U0bPI88A88PR_LHeuXaS4DWPnO-pUqpG5YUKie3vVr9id7M4pYpEm1-Es02txq_CLFQ7Lq_n3xE6R8WTz9UevO0ywK3ZS37nzUA&lib=MnQYBkRsDbv4uLRxNoSIgA-aoJlzzZ8rm";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZm5iMI0bDPnES6Hhkbev0HmylFwDtcHFQXZZ6LVdlqBX7OAS5ySLLLLqB2qpr-MjS/exec";
 
 function getProdutosUrl() {
     if (window.location.protocol === "file:") {
